079bb6738200e8ebb812dc4dd782dbe2f5200064
SimoneZoroddu <lenviur@gmail.com>
2026-02-12 18:02:17 +0100

    end of milestone 2

--- Milestone 1/main.js
+++ Milestone 1/main.js
@@ -1,16 +1,16 @@
-// diamo delle classi associate agli input e al bottonee
-
+// MILESTONE 1
+/* // diamo delle classi associate agli input e al bottonee
 
 const inputKmEl = document.querySelector('.inputKm')
 const inputAgeEl = document.querySelector('.inputAge')
 const buttonEl = document.querySelector('button')
-/* // proviamo se funzionano nei log
-inputKmEl.value = 10
-inputAgeEl.value = 10
-buttonEl.value = 10
-console.log(buttonEl);
-console.log(inputKmEl.value, inputAgeEl.value);
- */
+// proviamo se funzionano nei log
+//inputKmEl.value = 10
+//inputAgeEl.value = 10
+//buttonEl.value = 10
+//console.log(buttonEl);
+//console.log(inputKmEl.value, inputAgeEl.value);
+ 
 
 buttonEl.addEventListener('click', function (){
     const priceTicket = Number(inputKmEl.value) * 0.21
@@ -25,34 +25,38 @@ buttonEl.addEventListener('click', function (){
         console.log(priceTicket);
     }
 })
+ */
 
-
-/* 
+// MILESTONE 2
 // diamo delle classi associate agli input e al bottonee
-
 
 const inputKmEl = document.querySelector('.inputKm')
 const inputAgeEl = document.querySelector('.inputAge')
 const formEl = document.querySelector('form')
-/* // proviamo se funzionano nei log
-inputKmEl.value = 10
-inputAgeEl.value = 10
-formEl.value = 10
-console.log(inputKmEl.value, inputAgeEl.value, formEl.value);
- 
+// proviamo se funzionano nei log
+//inputKmEl.value = 10
+//inputAgeEl.value = 10
+//formEl.value = 10
+//console.log(inputKmEl.value, inputAgeEl.value, formEl.value);
+const recapDataEl = document.querySelector('.recapData')
+const priceEl = document.querySelector('.price')
 
 formEl.addEventListener('submit', function (e){
     e.preventDefault()
+    recapDataEl.innerHTML = `${inputKmEl.value}Km, ${inputAgeEl.value} Your Age`
+    
     const priceTicket = Number(inputKmEl.value) * 0.21
     const underAgeSale = priceTicket * 0.8
     const overAgeSale = priceTicket * 0.6
 
     if (Number(inputAgeEl.value) < 18) {
-        console.log(underAgeSale.toFixed(2));
+        //console.log(underAgeSale.toFixed(2));
+        priceEl.innerHTML = `${underAgeSale.toFixed(2)}&euro;`;
     } else if (Number(inputAgeEl.value) >= 65) {
-        console.log(overAgeSale.toFixed(2));
+        //console.log(overAgeSale.toFixed(2));
+        priceEl.innerHTML = `${overAgeSale.toFixed(2)}&euro;`;
     } else {
-        console.log(priceTicket);
+        priceEl.innerHTML = `${priceTicket}&euro;`;
+        //console.log(priceTicket);
     }
 })
- */
