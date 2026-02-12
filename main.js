2bc1e5f5eb1e14be9f71879aa26a58ee04fd00a0
SimoneZoroddu <lenviur@gmail.com>
2026-02-12 15:52:00 +0100

    variable for all class that i need

--- main.js
+++ main.js
@@ -13,3 +13,22 @@ value che viene importanto in questi campi
     tipologyTicket
     priceTicket
 */
+
+// iniziamo a prenderci le classi tramite delle variabili
+
+const nameFieldEl = document.querySelector('.nameField')
+/*  prova
+nameFieldEl.value = 'Simone'
+console.log(nameFieldEl.value);
+ */ // funziona
+const kmFieldEl = document.querySelector('.kmField')
+const opt1El = document.querySelector('.opt1')
+const opt2El = document.querySelector('.opt2')
+const opt3El = document.querySelector('.opt3')
+//bottoni
+const calcTicketEl = document.querySelector('.calcTicket')
+const clearAllEl = document.querySelector('.clearAll')
+//value di arrivo
+const nameTicketEl = document.querySelector('.nameTicket')
+const tipologyTicketEl = document.querySelector('.tipologyTicket')
+const priceTicketEl = document.querySelector('.priceTicket')
