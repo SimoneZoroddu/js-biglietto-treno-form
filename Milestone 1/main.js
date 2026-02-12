b27405555122194298aafa4bb091f687d254be58
SimoneZoroddu <lenviur@gmail.com>
2026-02-12 17:25:30 +0100

    price for KM

--- Milestone 1/main.js
+++ Milestone 1/main.js
@@ -3,3 +3,14 @@
 
 const inputKmEl = document.querySelector('.inputKm')
 const inputAgeEl = document.querySelector('.inputAge')
+const formEl = document.querySelector('form')
+/* // proviamo se funzionano nei log
+inputKmEl.value = 10
+inputAgeEl.value = 10
+formEl.value = 10
+console.log(inputKmEl.value, inputAgeEl.value, formEl.value);
+ */
+
+const priceTicket = Number(inputKmEl.value) * 0.21
+/* inputKmEl.value = 10
+console.log(priceTicket); */
