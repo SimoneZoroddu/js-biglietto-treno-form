7958864cc6b7eef23a1e8881a987a303f80d927a
SimoneZoroddu <lenviur@gmail.com>
2026-02-12 17:46:39 +0100

    adjust milestone 1 dont need form for the first milestone

--- Milestone 1/main.js
+++ Milestone 1/main.js
@@ -3,13 +3,43 @@
 
 const inputKmEl = document.querySelector('.inputKm')
 const inputAgeEl = document.querySelector('.inputAge')
+const buttonEl = document.querySelector('button')
+/* // proviamo se funzionano nei log
+inputKmEl.value = 10
+inputAgeEl.value = 10
+buttonEl.value = 10
+console.log(buttonEl);
+console.log(inputKmEl.value, inputAgeEl.value);
+ */
+
+buttonEl.addEventListener('click', function (){
+    const priceTicket = Number(inputKmEl.value) * 0.21
+    const underAgeSale = priceTicket * 0.8
+    const overAgeSale = priceTicket * 0.6
+    
+    if (Number(inputAgeEl.value) < 18) {
+        console.log(underAgeSale.toFixed(2));
+    } else if (Number(inputAgeEl.value) >= 65) {
+        console.log(overAgeSale.toFixed(2));
+    } else {
+        console.log(priceTicket);
+    }
+})
+
+
+/* 
+// diamo delle classi associate agli input e al bottonee
+
+
+const inputKmEl = document.querySelector('.inputKm')
+const inputAgeEl = document.querySelector('.inputAge')
 const formEl = document.querySelector('form')
 /* // proviamo se funzionano nei log
 inputKmEl.value = 10
 inputAgeEl.value = 10
 formEl.value = 10
 console.log(inputKmEl.value, inputAgeEl.value, formEl.value);
- */
+ 
 
 formEl.addEventListener('submit', function (e){
     e.preventDefault()
@@ -25,3 +55,4 @@ formEl.addEventListener('submit', function (e){
         console.log(priceTicket);
     }
 })
+ */
