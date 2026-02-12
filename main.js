f23c854405bd1fc5db1feea64a75e7bcd8a5d431
SimoneZoroddu <lenviur@gmail.com>
2026-02-12 15:44:57 +0100

    html style ready

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 /* console.log('collegato'); */ // test se funziona
+/* class di cui avro bisogno 
+value che scrive la persona
+    nameField
+    kmField
+    opt1/2/3
+button che mandano i dati (probabilmente non serve modificarlo perche metti l input sul suo genitore FORM)
+    calcTicket
+dovrebbe bastare un refresh della pagina su questo button
+    clearAll
+value che viene importanto in questi campi
+    nameTicket
+    tipologyTicket
+    priceTicket
+*/
